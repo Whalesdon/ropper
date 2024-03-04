e0056586718506e3cb573a35e4694451c59006aa
rahdeg <walett95@gmail.com>
2024-03-04 17:23:36 +0100

    ee

---
+++ static/js/325.281c01c4.chunk.js
@@ -0,0 +1,250 @@
+(self.webpackChunkcoti_marketing_new = self.webpackChunkcoti_marketing_new || []).push([
+    [325], {
+        6154: function(e, t, n) {
+            "use strict";
+            n.r(t), n.d(t, {
+                default: function() {
+                    return b
+                }
+            });
+            var i = n(1413),
+                o = n(7313),
+                a = (n(3151), n(1374), n(4026)),
+                s = n(7943),
+                r = n(3041),
+                c = n(407),
+                l = n(8437),
+                u = n(6417),
+                d = function(e) {
+                    var t = e.title,
+                        a = e.text,
+                        s = e.image,
+                        r = e.className,
+                        d = e.onClick,
+                        p = (0, l.C)(c.Ci).windowDimensions.isMobile,
+                        h = n(5802),
+                        m = s.includes("http") ? s : n(3005)("./".concat(s));
+                    return (0, o.useMemo)((function() {
+                        return (0, u.jsxs)("div", (0, i.Z)((0, i.Z)({
+                            className: "card_component ".concat(null !== r && void 0 !== r ? r : "")
+                        }, p ? {} : {
+                            onClick: function() {
+                                return d()
+                            }
+                        }), {}, {
+                            children: [(0, u.jsx)("img", {
+                                className: "card_image",
+                                src: m,
+                                alt: t,
+                                onError: function(e) {
+                                    e.currentTarget.onerror = null, e.currentTarget.src = h
+                                }
+                            }), (0, u.jsxs)("div", {
+                                className: "text_wrapper",
+                                children: [(0, u.jsx)("h2", {
+                                    children: t
+                                }), (0, u.jsx)("p", {
+                                    children: a
+                                })]
+                            }), (0, u.jsx)("div", {
+                                className: "app_icon arrow_right_icon",
+                                onClick: d
+                            })]
+                        }))
+                    }), [r, h, m, d, a, t, p])
+                },
+                p = [{
+                    title: "How COTI is Gearing Up for 2024 and Beyond",
+                    text: "Today we unveil a groundbreaking roadmap to establish ourselves as the first privacy-centric Ethereum Layer 2. Robust privacy protection is missing from Ethereum today, preventing countless valuable use cases from being developed. While transparency is a critical feature of blockchain, the inability to protect the information that should be private is harmful to users and to the potential growth of Web3 as an industry. Just as HTTP evolved to HTTPS, so must blockchains.",
+                    link: "https://cotinetwork.medium.com/how-coti-is-gearing-up-for-2024-and-beyond-e0d465794767 ",
+                    image: "coti2024.webp"
+                }, {
+                    title: "Unlocking Possibilities: Introducing the COTI Foundation!",
+                    text: "We are delighted to announce the launch of the COTI Foundation, an independent, non-profit organization passionately dedicated to driving the adoption and progression of the COTI network.",
+                    link: "https://medium.com/@cotinetwork/unlocking-possibilities-introducing-the-coti-foundation-4f3596cf7283",
+                    image: "coti_foundation.webp"
+                }, {
+                    title: "A New Era For Stablecoins Begins: Djed is Live on Mainnet!",
+                    text: "After over a year of preparations and development, and following a successful security audit, we are very proud to announce that Djed, the overcollateralized stablecoin on Cardano, is now live on mainnet!",
+                    link: "https://medium.com/cotinetwork/a-new-era-for-stablecoins-begins-djed-is-live-on-mainnet-55971971f2a8",
+                    image: "a-new-era-for-stablecoins-begins-djed-is-live-on-mainnet.png"
+                }, {
+                    title: "Lift Off! COTI\u2019s First PPN Has Successfully Soft-Launched Along with its First Token: $DPINR",
+                    text: "We are delighted to announce that after more than a year of intense development and rigorous testing, we have successfully launched our first client Private Payment Network (PPN) solution, and its token: Delta Platinum INR.",
+                    link: "https://medium.com/@cotinetwork/lift-off-cotis-first-ppn-has-successfully-soft-launched-along-with-its-first-token-dpinr-cb6d91c0d637",
+                    image: "coti_ppn.webp"
+                }, {
+                    title: "You Voted and Decided: New APY Boost of 1600% is Now Enabled!",
+                    text: "We are happy to share that starting now a new APY Boost of 1600% is available for gCOTI holders!",
+                    link: "https://medium.com/@cotinetwork/new-apy-boost-of-1600-is-now-enabled-0104752ee05d",
+                    image: "coti_apy_boost1600.webp"
+                }, {
+                    title: "MultiDAG 2.0 Public Mainnet is LIVE!",
+                    text: "We are proud to announce that MultiDAG 2.0 hard fork event was successfully completed and the Mainnet is now live! The event took place earlier today, during which Explorer 2.0 was also deployed to Mainnet, alongside the Bridge 2.0 wallet app.",
+                    link: "https://medium.com/cotinetwork/multidag-2-0-public-mainnet-is-live-f6a6ac7b6946",
+                    image: "multidag-2-0-public-mainnet-is-live.png"
+                }, {
+                    title: "COTI \u2014 The Enterprise Layer 1 \u2014 Some Thoughts About the Future",
+                    text: "Today marks the third anniversary of COTI\u2019s Trustchain Mainnet\u2019s launch as well as the COTI coin launch, which took place on June 4, 2019. During those three years, we have accomplished a lot.",
+                    link: "https://medium.com/cotinetwork/coti-the-enterprise-layer-1-some-thoughts-about-the-future-4f677904f483",
+                    image: "coti-the-enterprise-layer-1.png"
+                }, {
+                    title: "DJED Launch date",
+                    text: "As just announced by Shahaf Bar-Geffen on the main stage at the Cardano Summit, we are thrilled to share that Djed, Cardano\u2019s over-collateralized algorithmic stablecoin, will be live on mainnet this January, 2023 following a successful full audit.",
+                    link: "https://medium.com/cotinetwork/update-from-the-cardano-summit-main-stage-we-have-a-date-58024fdb67ac",
+                    image: "update-from-the-cardano-summit.png"
+                }, {
+                    title: "Binance Pay has added support for $COTI!",
+                    text: "We are pleased to announce that Binance Pay has added support for $COTI (ERC20), to enable millions of registered Binance users to send, receive and make payments in $COTI.",
+                    link: "https://medium.com/@cotinetwork/binance-pay-now-supports-coti-to-enable-borderless-crypto-payments-707bc398e08f",
+                    image: "binance-pay-now-supports-coti.png"
+                }, {
+                    title: "COTI x Revolut",
+                    text: "We are pleased to inform you that Revolut has added $COTI (ERC20) to its supported crypto offering in their app! Revolut is building the world\u2019s first global financial superapp.",
+                    link: "https://cotinetwork.medium.com/revolut-has-added-coti-to-its-financial-app-78d503cdf79b",
+                    image: "revolut-has-added-coti.png"
+                }, {
+                    title: "Binance Custody Will Become COTI\u2019s Custodian for its ERC20 Tokens",
+                    text: "We are happy to announce another step taken by COTI in furthering our partnership and collaboration with the Binance ecosystem.",
+                    link: "https://medium.com/cotinetwork/binance-custody-will-become-cotis-custodian-for-its-erc20-tokens-bfba6c3c9e3e",
+                    image: "binance-custody-will-become-cotis-custodian.png"
+                }, {
+                    title: "Why COTI\u2019s Proof of Trust is a Green Alternative to PoW",
+                    text: "On May 12th, 2021, Elon Musk tweeted from his official handle about Tesla\u2019s decision to suspend \u201cvehicle purchases using Bitcoin.",
+                    link: "https://medium.com/cotinetwork/why-cotis-proof-of-trust-is-a-green-alternative-to-pow-cceb5620697",
+                    image: "why-cotis-proof-of-trust-is-a-green.png"
+                }],
+                h = n(4888),
+                m = n(5369),
+                f = n(6031),
+                g = n.n(f),
+                w = {
+                    speed: 1500,
+                    scrollbar: !1,
+                    pagination: {
+                        clickable: !0,
+                        dynamicBullets: !0,
+                        dynamicMainBullets: 1
+                    },
+                    modules: [s.pt, s.tl, s.W_]
+                },
+                b = function(e) {
+                    var t = e.id,
+                        n = (0, o.useCallback)((function() {
+                            return "grid" === t ? (0, u.jsx)(v, {}) : (0, u.jsxs)(u.Fragment, {
+                                children: [(0, u.jsx)("h2", {
+                                    className: "title",
+                                    children: "COTI's News"
+                                }), (0, u.jsx)(k, {})]
+                            })
+                        }), [t]);
+                    return (0, u.jsx)(r.Z, {
+                        className: "blog_carousel_component",
+                        children: n()
+                    })
+                },
+                k = function() {
+                    var e = (0, l.C)(c.Ci).windowDimensions,
+                        t = e.isMobile,
+                        n = e.isPortraitTablet,
+                        s = e.isTablet,
+                        r = (0, o.useMemo)((function() {
+                            return t ? 1 : n || s ? 2 : 3
+                        }), [t, n, s]);
+                    return (0, o.useMemo)((function() {
+                        return (0, u.jsx)(a.tq, (0, i.Z)((0, i.Z)({
+                            className: "blog_carousel"
+                        }, w), {}, {
+                            slidesPerView: r,
+                            slidesPerGroup: r,
+                            spaceBetween: 24,
+                            navigation: r < p.length,
+                            children: p.map((function(e, t) {
+                                return (0, u.jsx)(a.o5, {
+                                    children: (0, u.jsx)(d, {
+                                        title: e.title,
+                                        text: e.text,
+                                        image: e.image,
+                                        onClick: function() {
+                                            (0, h.YQ)(e.link), (0, m.Nf)('[Blog] "'.concat(e.title, '" button click'))
+                                        }
+                                    })
+                                }, t)
+                            }))
+                        }))
+                    }), [r])
+                },
+                v = function() {
+                    var e = (0, l.C)(c.Ci).windowDimensions.isMobile,
+                        t = (0, o.useMemo)((function() {
+                            return e ? 3 : 8
+                        }), [e]),
+                        n = (0, o.useMemo)((function() {
+                            return g().chunk(p, t)
+                        }), [t]);
+                    return (0, o.useMemo)((function() {
+                        return (0, u.jsx)(a.tq, (0, i.Z)((0, i.Z)({
+                            className: "blog_grid_carousel"
+                        }, w), {}, {
+                            slidesPerView: 1,
+                            slidesPerGroup: 1,
+                            spaceBetween: 0,
+                            navigation: t < p.length,
+                            speed: 500,
+                            autoHeight: !0,
+                            noSwiping: !0,
+                            noSwipingClass: "swiper-slide",
+                            children: n.map((function(e, t) {
+                                return (0, u.jsx)(a.o5, {
+                                    children: e.map((function(e, n) {
+                                        return (0, u.jsx)(d, {
+                                            title: e.title,
+                                            text: e.text,
+                                            image: e.image,
+                                            onClick: function() {
+                                                (0, h.YQ)(e.link), (0, m.Nf)('[Blog] "'.concat(e.title, '" button click'))
+                                            }
+                                        }, "".concat(t, "-").concat(n))
+                                    }))
+                                }, t)
+                            }))
+                        }))
+                    }), [n, t])
+                }
+        },
+        3005: function(e, t, n) {
+            var i = {
+                "./a-new-era-for-stablecoins-begins-djed-is-live-on-mainnet.png": 1081,
+                "./binance-custody-will-become-cotis-custodian.png": 4524,
+                "./binance-pay-now-supports-coti.png": 8435,
+                "./coti-the-enterprise-layer-1.png": 2815,
+                "./coti2024.webp": 3346,
+                "./coti_apy_boost1600.webp": 5147,
+                "./coti_foundation.webp": 9864,
+                "./coti_ppn.webp": 8149,
+                "./cotis-bridge-2-0-is-now-live.png": 9251,
+                "./multidag-2-0-hard-fork-testnet.png": 2405,
+                "./multidag-2-0-public-mainnet-is-live.png": 4346,
+                "./revolut-has-added-coti.png": 418,
+                "./update-from-the-cardano-summit.png": 1426,
+                "./why-cotis-proof-of-trust-is-a-green.png": 5326
+            };
+
+            function o(e) {
+                var t = a(e);
+                return n(t)
+            }
+
+            function a(e) {
+                if (!n.o(i, e)) {
+                    var t = new Error("Cannot find module '" + e + "'");
+                    throw t.code = "MODULE_NOT_FOUND", t
+                }
+                return i[e]
+            }
+            o.keys = function() {
+                return Object.keys(i)
+            }, o.resolve = a, e.exports = o, o.id = 3005
+        }
+    }
+]);
